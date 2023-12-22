10884a713cb6d2f6415aeaaeda1f44a39ef62dc7
yiminchen1999 <90345634+yimin1019@users.noreply.github.com>
2023-12-22 03:07:07 -0500

    add more data to db

--- exercise_look_up.py
+++ exercise_look_up.py
@@ -1,34 +1,43 @@
 import graphene
-from graphene import ObjectType, String, Float, List, Schema, Int
+from graphene import ObjectType, String, Float, List, Schema
 
-# Exercise Database
+# ref: https://www.mayoclinic.org/healthy-lifestyle/weight-loss/in-depth/exercise/art-20050999
 EXERCISE_LOOKUP_DB = {
-    1: {"exercise_type": "running", "calories_burned_per_minute": 10},
-    2: {"exercise_type": "cycling", "calories_burned_per_minute": 8},
-    3: {"exercise_type": "swimming", "calories_burned_per_minute": 9},
+    "running": {"calories_burned_per_minute": 606 / 60},  # 10.1 calories per minute
+    "cycling": {"calories_burned_per_minute": 292 / 60},  # 4.87 calories per minute
+    "swimming": {"calories_burned_per_minute": 423 / 60},  # 7.05 calories per minute
+    "yoga": {"calories_burned_per_minute": 183 / 60},  # 3.05 calories per minute (moderate effort)
+    "weightlifting": {"calories_burned_per_minute": 108 / 60},  # 1.8 calories per minute
+    "aerobics_low_impact": {"calories_burned_per_minute": 365 / 60},  # 6.08 calories per minute
+    "aerobics_water": {"calories_burned_per_minute": 402 / 60},  # 6.7 calories per minute
+    "dancing_ballroom": {"calories_burned_per_minute": 219 / 60},  # 3.65 calories per minute
+    "elliptical_trainer": {"calories_burned_per_minute": 365 / 60},  # 6.08 calories per minute
+    "golfing": {"calories_burned_per_minute": 314 / 60},  # 5.23 calories per minute
+    "hiking": {"calories_burned_per_minute": 438 / 60},  # 7.3 calories per minute
+    "skiing_downhill": {"calories_burned_per_minute": 314 / 60},  # 5.23 calories per minute
+    "walking": {"calories_burned_per_minute": 314 / 60}  # 5.23 calories per minute (3.5 mph)
 }
 
 # ExerciseItem Type
 class ExerciseItem(ObjectType):
-    exercise_index = Int()
     exercise_type = String()
     calories_burned_per_minute = Float()
 
-# Exercise Query Class
+# Query Class
 class ExerciseQuery(ObjectType):
-    get_exercise_item = graphene.Field(ExerciseItem, exercise_index=Int())
-    list_all_exercises = List(ExerciseItem)
-
-    def resolve_get_exercise_item(root, info, exercise_index):
-        exercise_data = EXERCISE_LOOKUP_DB.get(exercise_index)
-        if exercise_data:
-            return ExerciseItem(exercise_index=exercise_index, **exercise_data)
-        return None
+    list_all_exercises = List(String)
+    get_calories_burned = graphene.Field(Float, exercise_type=String())
 
     def resolve_list_all_exercises(root, info):
-        return [ExerciseItem(exercise_index=index, **data) for index, data in EXERCISE_LOOKUP_DB.items()]
+        return list(EXERCISE_LOOKUP_DB.keys())
 
-# Exercise Mutation Class
+    def resolve_get_calories_burned(root, info, exercise_type):
+        exercise_data = EXERCISE_LOOKUP_DB.get(exercise_type)
+        if exercise_data:
+            return exercise_data['calories_burned_per_minute']
+        return None
+
+# Mutation Class
 class CreateExerciseItem(graphene.Mutation):
     class Arguments:
         exercise_type = String(required=True)
@@ -37,64 +46,29 @@ class CreateExerciseItem(graphene.Mutation):
     exercise_item = graphene.Field(ExerciseItem)
 
     def mutate(self, info, exercise_type, calories_burned_per_minute):
-        new_index = max(EXERCISE_LOOKUP_DB.keys()) + 1
-        EXERCISE_LOOKUP_DB[new_index] = {"exercise_type": exercise_type, "calories_burned_per_minute": calories_burned_per_minute}
-        return CreateExerciseItem(exercise_item=ExerciseItem(exercise_index=new_index, exercise_type=exercise_type, calories_burned_per_minute=calories_burned_per_minute))
+        EXERCISE_LOOKUP_DB[exercise_type] = {"calories_burned_per_minute": calories_burned_per_minute}
+        return CreateExerciseItem(exercise_item=ExerciseItem(exercise_type=exercise_type, calories_burned_per_minute=calories_burned_per_minute))
 
-# Exercise Mutation ObjectType
+# Mutation ObjectType
 class ExerciseMutation(ObjectType):
     create_exercise_item = CreateExerciseItem.Field()
 
-# Exercise Schema
+# Schema
 exercise_schema = Schema(query=ExerciseQuery, mutation=ExerciseMutation)
 
-# Executing a Query to Get a Specific Exercise Item
-exercise_query_string = '''
-{
-    getExerciseItem(exerciseIndex: 1) {
-        exerciseType
-        caloriesBurnedPerMinute
-    }
-}
-'''
-result = exercise_schema.execute(exercise_query_string)
+# Executing Queries
+# Query to List All Exercise Types
+query_all_exercises = '{ listAllExercises }'
+result = exercise_schema.execute(query_all_exercises)
 if result.errors:
     print("Errors:", result.errors)
 else:
-    print("Exercise item details:", result.data['getExerciseItem'])
+    print("All exercise types:", result.data['listAllExercises'])
 
-# Executing a Mutation to Add a New Exercise Item
-exercise_mutation_string = '''
-mutation {
-    createExerciseItem(exerciseType: "yoga", caloriesBurnedPerMinute: 5) {
-        exerciseItem {
-            exerciseType
-            caloriesBurnedPerMinute
-        }
-    }
-}
-'''
-result = exercise_schema.execute(exercise_mutation_string)
+# Query to Get Calorie Burn Rate for a Specific Exercise Type
+query_calories = '{ getCaloriesBurned(exerciseType: "running") }'
+result = exercise_schema.execute(query_calories)
 if result.errors:
     print("Errors:", result.errors)
 else:
-    print("Added exercise item:", result.data['createExerciseItem']['exerciseItem'])
-
-# Executing a Query to List All Exercise Items
-exercise_query_all_string = '''
-{
-    listAllExercises {
-        exerciseIndex
-        exerciseType
-        caloriesBurnedPerMinute
-    }
-}
-'''
-result = exercise_schema.execute(exercise_query_all_string)
-if result.errors:
-    print("Errors:", result.errors)
-else:
-    print("All exercise items:")
-    for item in result.data['listAllExercises']:
-        print(f"Index {item['exerciseIndex']}: {item['exerciseType'].title()}, {item['caloriesBurnedPerMinute']} calories/min")
-
+    print(result.data['getCaloriesBurned'])
--- food_look_up.py
+++ food_look_up.py
@@ -1,32 +1,76 @@
 import graphene
-from graphene import ObjectType, String, Float, List, Schema, Int
+from graphene import ObjectType, String, Float, List, Schema
 
-# Database
+#ref: https://us.myprotein.com/thezone/nutrition/food-calories-chart-whats-in-your-fruit-veg-meat-and-other-daily-produce/
 FOOD_LOOKUP_DB = {
-    1: {"food_type": "apple", "unit_calorie_g": 0.52},
-    2: {"food_type": "banana", "unit_calorie_g": 0.89},
-    3: {"food_type": "orange", "unit_calorie_g": 0.47},
+    "apple": {"unit_calorie_g": 0.37},  # 37 kcal per 100g
+    "banana": {"unit_calorie_g": 0.51},  # 51 kcal per 100g
+    "orange": {"unit_calorie_g": 0.39},  # Approximate value
+    "rice_brown": {"unit_calorie_g": 1.32},  # 132 kcal per 100g (cooked)
+    "chicken_breast": {"unit_calorie_g": 1.48},  # 148 kcal per 100g
+    "broccoli": {"unit_calorie_g": 0.35},  # 35 kcal per 100g
+    "carrot": {"unit_calorie_g": 0.10},  # 10 kcal per 100g
+    "cheddar_cheese": {"unit_calorie_g": 4.03},  # 403 kcal per 100g
+    "salmon_raw": {"unit_calorie_g": 1.83},  # 183 kcal per 100g
+    "egg_whole": {"unit_calorie_g": 1.55},  # 155 kcal per 100g
+    "almonds": {"unit_calorie_g": 5.75},  # 575 kcal per 100g
+    "pasta_cooked": {"unit_calorie_g": 1.58},
+    "spinach": {"unit_calorie_g": 0.24},  # 24 kcal per 100g
+    "avocado": {"unit_calorie_g": 1.34},  # 134 kcal per 100g
+    "tofu": {"unit_calorie_g": 0.76},  # 76 kcal per 100g
+    "potato": {"unit_calorie_g": 0.97},  # 97 kcal per 100g
+    "beef_steak": {"unit_calorie_g": 2.52},  # 252 kcal per 100g
+    "whole_milk": {"unit_calorie_g": 0.62},  # 62 kcal per 100mL
+    "oats": {"unit_calorie_g": 3.81},  # 381 kcal per 100g (rolled oats)
+    "peanut_butter": {"unit_calorie_g": 5.94},
+    "quinoa": {"unit_calorie_g": 1.11},  # 111 kcal per 100g (cooked)
+    "lentils": {"unit_calorie_g": 1.16},  # 116 kcal per 100g (cooked)
+    "chickpeas": {"unit_calorie_g": 1.64},  # 164 kcal per 100g (cooked)
+    "turkey_breast": {"unit_calorie_g": 1.04},  # 104 kcal per 100g
+    "salmon": {"unit_calorie_g": 2.08},  # 208 kcal per 100g
+    "sweet_potato": {"unit_calorie_g": 0.86},  # 86 kcal per 100g
+    "blueberries": {"unit_calorie_g": 0.57},  # 57 kcal per 100g
+    "almond_milk": {"unit_calorie_g": 0.17},
+    "blackberries": {"unit_calorie_g": 0.21},  # 21 kcal per 100g
+    "cucumber": {"unit_calorie_g": 0.15},  # 15 kcal per 100g
+    "tomato": {"unit_calorie_g": 0.18},  # 18 kcal per 100g
+    "mushroom": {"unit_calorie_g": 0.08},  # 8 kcal per 100g
+    "cauliflower": {"unit_calorie_g": 0.30},  # 30 kcal per 100g
+    "green_peas": {"unit_calorie_g": 0.70},  # 70 kcal per 100g
+    "pumpkin": {"unit_calorie_g": 0.13},
+    "strawberries": {"unit_calorie_g": 0.32},  # 32 kcal per 100g
+    "spinach": {"unit_calorie_g": 0.23},  # 23 kcal per 100g
+    "walnuts": {"unit_calorie_g": 6.54},  # 654 kcal per 100g
+    "cod": {"unit_calorie_g": 0.82},  # 82 kcal per 100g
+    "shrimp": {"unit_calorie_g": 0.99},  # 99 kcal per 100g
+    "beef": {"unit_calorie_g": 2.50},  # 250 kcal per 100g
+    "pork": {"unit_calorie_g": 2.42},
+    "olive_oil": {"unit_calorie_g": 8.80},  # 880 kcal per 100g
+    "bread_whole_grain": {"unit_calorie_g": 2.66},  # 266 kcal per 100g
+    "yogurt_plain": {"unit_calorie_g": 0.59},  # 59 kcal per 100g
+    "lamb": {"unit_calorie_g": 2.94},  # 294 kcal per 100g
+    "honey": {"unit_calorie_g": 3.04},  # 304 kcal per 100g
+    "cheddar_cheese": {"unit_calorie_g": 4.03}
 }
 
 # FoodItem Type
 class FoodItem(ObjectType):
-    food_index = Int()
     food_type = String()
     unit_calorie_g = Float()
 
 # Query Class
 class Query(ObjectType):
-    get_food_item = graphene.Field(FoodItem, food_index=Int())
-    list_all_foods = List(FoodItem)
-
-    def resolve_get_food_item(root, info, food_index):
-        food_data = FOOD_LOOKUP_DB.get(food_index)
-        if food_data:
-            return FoodItem(food_index=food_index, **food_data)
-        return None
+    list_all_foods = List(String)
+    get_unit_calories = graphene.Field(Float, food_type=String())
 
     def resolve_list_all_foods(root, info):
-        return [FoodItem(food_index=index, **data) for index, data in FOOD_LOOKUP_DB.items()]
+        return list(FOOD_LOOKUP_DB.keys())
+
+    def resolve_get_unit_calories(root, info, food_type):
+        food_data = FOOD_LOOKUP_DB.get(food_type)
+        if food_data:
+            return food_data['unit_calorie_g']
+        return None
 
 # Mutation Class
 class CreateFoodItem(graphene.Mutation):
@@ -37,63 +81,32 @@ class CreateFoodItem(graphene.Mutation):
     food_item = graphene.Field(FoodItem)
 
     def mutate(self, info, food_type, unit_calorie_g):
-        new_index = max(FOOD_LOOKUP_DB.keys()) + 1
-        FOOD_LOOKUP_DB[new_index] = {"food_type": food_type, "unit_calorie_g": unit_calorie_g}
-        return CreateFoodItem(food_item=FoodItem(food_index=new_index, food_type=food_type, unit_calorie_g=unit_calorie_g))
+        FOOD_LOOKUP_DB[food_type] = {"unit_calorie_g": unit_calorie_g}
+        return CreateFoodItem(food_item=FoodItem(food_type=food_type, unit_calorie_g=unit_calorie_g))
 
 # Mutation ObjectType
 class Mutation(ObjectType):
     create_food_item = CreateFoodItem.Field()
+
 # Schema
 schema = Schema(query=Query, mutation=Mutation)
 
-# Executing a Query to Get a Specific Food Item
-query_string = '''
-{
-    getFoodItem(foodIndex: 1) {
-        foodType
-        unitCalorieG
-    }
-}
-'''
-result = schema.execute(query_string)
-if result.errors:
-    print("Errors:", result.errors)
-else:
-    print("Food item details:", result.data['getFoodItem'])
-
-# Executing a Mutation to Add a New Food Item
-mutation_string = '''
-mutation {
-    createFoodItem(foodType: "mango", unitCalorieG: 0.60) {
-        foodItem {
-            foodType
-            unitCalorieG
-        }
-    }
-}
-'''
-result = schema.execute(mutation_string)
-if result.errors:
-    print("Errors:", result.errors)
-else:
-    print("Added food item:", result.data['createFoodItem']['foodItem'])
-
-# Executing a Query to List All Food Items
-query_all_foods = '''
-{
-    listAllFoods {
-        foodIndex
-        foodType
-        unitCalorieG
-    }
-}
-'''
+# Executing Queries
+# Query to List All Food Names
+query_all_foods = '{ listAllFoods }'
 result = schema.execute(query_all_foods)
 if result.errors:
     print("Errors:", result.errors)
 else:
-    print("All food items:")
-    for item in result.data['listAllFoods']:
-        print(f"Index {item['foodIndex']}: {item['foodType'].title()}, {item['unitCalorieG']} calories/g")
+    print("All food types:", result.data['listAllFoods'])
 
+# Schema
+schema = Schema(query=Query, mutation=Mutation)
+
+# Executing a Query to Get Calories for a Specific Food Type
+query_calories = '{ getUnitCalories(foodType: "apple") }'
+result = schema.execute(query_calories)
+if result.errors:
+    print("Errors:", result.errors)
+else:
+    print(result.data['getUnitCalories'])
